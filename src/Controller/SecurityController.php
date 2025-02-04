1da13e035b1d2e54f75c6d955a63d08239c42c34
Ewillian9 <janonguittard@gmail.com>
2025-02-04 16:45:17 +0100

    useragent, login history, login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,21 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Mime\Email;
-use App\Repository\UserRepository;
+use App\Entity\LoginHistory;
+use DeviceDetector\DeviceDetector;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request, EntityManagerInterface $em): Response
     {
+
+        $userAgent = $request->headers->get('User-Agent');
+        $deviceDetector = new DeviceDetector($userAgent);
+        $deviceDetector->parse();
+
+        if ($this->getUser()) {
+            $loginHistory = new LoginHistory();
+            $loginHistory
+                ->setUser($this->getUser())
+                ->setIpAddress($request->getClientIp())
+                ->setDevice($deviceDetector->getDeviceName())
+                ->setOs($deviceDetector->getOs()['name'])
+                ->setBrowser($deviceDetector->getClient()['name']);
+            $em->persist($loginHistory);
+            $em->flush();
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -27,39 +44,6 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-    #[Route('/login', name: 'app_login')]
-    public function requestLoginLink(LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request, MailerInterface $mailer): Response
-    {
-        // check if form is submitted
-        if ($request->isMethod('POST')) {
-            // load the user in some way (e.g. using the form input)
-            $email = $request->getPayload()->get('email');
-            $user = $userRepository->findOneBy(['email' => $email]);
-
-            // create a login link for $user this returns an instance
-            // of LoginLinkDetails
-            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
-            $loginLink = $loginLinkDetails->getUrl();
-
-            // ... send the link and return a response (see next section)
-            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
-            $loginLink = $loginLinkDetails->getUrl();
-
-            $email = (new Email())
-            ->from('contact@miniamaker.fr')
-            ->to($user->getEmail())
-            ->priority(Email::PRIORITY_HIGH)
-            ->subject('Please Confirm your Email')
-            ->text('Votre lien de connexion')
-            ->html("<p>Click : <br>" . $loginLink . "</p>");
-
-            $mailer->send($email);
-        }
-
-        // if it's not submitted, render the form to request the "login link"
-        return $this->render('security/login.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
