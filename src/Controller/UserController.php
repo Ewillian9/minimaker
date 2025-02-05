0d8494756761d17035ea7d202412ff79d1bb70c4
Ewillian9 <janonguittard@gmail.com>
2025-02-05 16:56:30 +0100

    merge jensone

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +16,21 @@ final class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/complete', name: 'app_complete', methods: ['POST'])]
+    public function complete(Request $request, EntityManagerInterface $em): Response
+    {
+        $data = $request->getPayload();
+        if (!empty($data->get('username')) && !empty($data->get('fullname'))) {
+            $user = $this->getUser();
+            $user
+                ->setUsername($data->get('username'))
+                ->setFullname($data->get('fullname'));
+            $em->persist($user);
+            $em->flush($user);
+        }
+        $this->addFlash('success', 'Votre profil est complété');
+        return $this->redirectToRoute('app_profile');
     }
 }
