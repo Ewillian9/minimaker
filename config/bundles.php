a88cf3a8c12b2f095dab81d85de82b0fbe22c1b2
Ewillian9 <janonguittard@gmail.com>
2025-02-10 16:44:17 +0100

    insyall liip/imagine

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
