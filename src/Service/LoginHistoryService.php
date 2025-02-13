4b83c27fb803ea525a3d04efd90cbb6fa8ae94ae
Ewillian9 <janonguittard@gmail.com>
2025-02-13 12:00:41 +0100

    send email on login

--- src/Service/LoginHistoryService.php
+++ src/Service/LoginHistoryService.php
@@ -4,16 +4,24 @@ namespace App\Service;
 
 use App\Entity\User;
 use App\Entity\LoginHistory;
+use Symfony\Component\Mime\Email;
 use DeviceDetector\DeviceDetector;
+use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Classe de gestion de l'historique de connexion des utilisateurs
  */
 
-class LoginHistoryService
+class LoginHistoryService extends AbstractController
 {
-    public function __construct(readonly private EntityManagerInterface $em){}
+    public function __construct(readonly private EntityManagerInterface $em, MailerInterface $mailer){
+        $this->mailer = $mailer;
+    }
 
     public function addHistory(User $user, string $userAgent, string $ip): void
     {
@@ -30,5 +38,13 @@ class LoginHistoryService
             ;
         $this->em->persist($loginHistory);
         $this->em->flush();
+
+        $email = (new TemplatedEmail())
+            ->from(new Address('contact@miniamaker.fr', 'miniamaker'))
+            ->to((string) $user->getEmail())
+            ->subject('Nouvelle connexion détéctée')
+            ->htmlTemplate('security/login_email.html.twig');
+
+        $this->mailer->send($email);
     }
 }
